9f748a6d33fbbeec512b8e418782b7287488c111
RenanSilveira <renanesilveira@hotmail.com>
2025-02-26 20:52:11 -0300

    publicando um erro que tinha os metodos de string no arquivo de datatypes

--- src/datatypes.py
+++ src/datatypes.py
@@ -9,22 +9,4 @@ valorBooleano = True or False #type: ignore
 valorLista = [0,1,2,3,4,5] 
 valorDicionario = {'key0': 'valor0', 'key1': 'valor1'}
 
-# STRING METHODS #
-upper = valorTexto.upper() #converte o texto em uppercase
-#EH SÓ UM TEXTO KKKK
-
-lower = valorTexto.lower() #converte o texto em lowercase
-#eh só um texto kkkk
-
-title = valorTexto.title() #converte em title case (primeira maiuscula de cada palavra)
-#Eh Só Um Texto Kkkk
-
-stringCount = valorTexto.count('e') #conta quantas letras E tem no texto
-#2
-
-stringFind = valorTexto.find('e') #posição da letra pesquisada na primeira ocorrencia
-#0
-
-stringReplace = valorTexto.replace('h', '') #troca as ocorrencias do primeiro parametro pela do segundo parametro
-print(stringReplace)
 
