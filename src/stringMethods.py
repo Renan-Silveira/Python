e170394bffbe6fde6edb870b4aba986ee2d26d1c
RenanSilveira <renanesilveira@hotmail.com>
2025-02-26 20:53:38 -0300

    publicando um erro que tinha os metodos de string no arquivo de datatypes

---
+++ src/stringMethods.py
@@ -0,0 +1,20 @@
+from datatypes import valorTexto #um import para usar a mesma variavel
+
+# STRING METHODS #
+upper = valorTexto.upper() #converte o texto em uppercase
+#EH SÓ UM TEXTO KKKK
+
+lower = valorTexto.lower() #converte o texto em lowercase
+#eh só um texto kkkk
+
+title = valorTexto.title() #converte em title case (primeira maiuscula de cada palavra)
+#Eh Só Um Texto Kkkk
+
+stringCount = valorTexto.count('e') #conta quantas letras E tem no texto
+#2
+
+stringFind = valorTexto.find('e') #posição da letra pesquisada na primeira ocorrencia
+#0
+
+stringReplace = valorTexto.replace('h', '') #troca as ocorrencias do primeiro parametro pela do segundo parametro
+#e só um texto kkkk
