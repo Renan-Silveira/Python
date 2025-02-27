f85be8d19188b03f77c1b40b2d4f2772d6939ac7
RenanSilveira <renanesilveira@hotmail.com>
2025-02-26 21:06:02 -0300

    publicando algumas alterações de f string e de operadores

--- src/datatypes.py
+++ src/datatypes.py
@@ -1,10 +1,27 @@
 # DATATYPES #
 #o motivo desse arquivo é para que tenha registro do basico do python no meu github :)
 
+#operadores numericos
+# +  | adição
+# -  | subtração
+# *  | multiplicação
+# /  | divisão
+# ** | exponenciação
+# %  | modulação geralmente usada para atribuir valor a alguma string
+# // | divisão por arredondamento
+
+#operadores de comparação
+# == | igual à
+# != | diferente
+# >  | maior que
+# <  | menor que
+# >= | maior ou igual que
+# <= | menor ou igual que
+
 #integers
 valorInteiro = 1
 valorDecimal =  1.2
-valorTexto =  str('eh só um texto kkkk') # type: ignore
+valorTexto =  str('eh só um texto kkkk')
 valorBooleano = True or False #type: ignore
 valorLista = [0,1,2,3,4,5] 
 valorDicionario = {'key0': 'valor0', 'key1': 'valor1'}
--- src/stringMethods.py
+++ src/stringMethods.py
@@ -2,19 +2,35 @@ from datatypes import valorTexto #um import para usar a mesma variavel
 
 # STRING METHODS #
 upper = valorTexto.upper() #converte o texto em uppercase
-#EH SÓ UM TEXTO KKKK
+#print EH SÓ UM TEXTO KKKK
 
 lower = valorTexto.lower() #converte o texto em lowercase
-#eh só um texto kkkk
+#print eh só um texto kkkk
 
 title = valorTexto.title() #converte em title case (primeira maiuscula de cada palavra)
-#Eh Só Um Texto Kkkk
+#print Eh Só Um Texto Kkkk
 
 stringCount = valorTexto.count('e') #conta quantas letras E tem no texto
-#2
+#print 2
 
 stringFind = valorTexto.find('e') #posição da letra pesquisada na primeira ocorrencia
-#0
+#print 0
 
 stringReplace = valorTexto.replace('h', '') #troca as ocorrencias do primeiro parametro pela do segundo parametro
-#e só um texto kkkk
+#print e só um texto kkkk
+
+#conectando strings#
+mensagem_1  = "eu estou aprendendo python"
+mensagem_2= "e é divertido"
+
+#concatenando strings com operador +
+mensagem = mensagem_1 + ' ' + mensagem_2 #concatena com uma string de espaço e com a segunda variavel
+#print eu estou aprendendo python e é divertido
+
+#concatenando com f-string
+mensagemFString = f'{mensagem_1} {mensagem_2}'
+#print eu estou aprendendo python e é divertido
+
+#concatenando com valores atribuidos
+mensagemAtribuida = '%s %s' %(mensagem_1,mensagem_2)
+#print eu estou aprendendo python e é divertido
