b6222784940e131c675b90aef1260533db06d086
RenanSilveira <renanesilveira@hotmail.com>
2025-02-26 21:16:06 -0300

    criei o arquivo de tratamento de listas e está na metade

---
+++ src/listMethods.py
@@ -0,0 +1,27 @@
+#criando uma lista
+paises = ['USA', 'India', 'China', 'Brazil'] #lembrando que o indice sempre começa com 0
+#[0,1,2,3,etc]
+
+#criando uma lista vazia
+listaVazia = []
+
+#indexação de uma lista
+paises[0] #retornando o valor na casa 0 da lista
+#print USA
+
+paises[3] #retornando o valor na casa 3 da lista
+#print Brazil
+
+paises[2] #retornando o valor na casa 2 da lista
+#print China
+
+
+#cortando uma lista
+paises[0:2] #retorna paises na casa 0 até a casa 1
+#print ['USA', 'India']
+
+paises[1:] #retorna a lista a partir do indice 1
+#print ['India', 'China', 'Brazil']
+
+paises[:2] #retorna a lista até o indice 1, o indice final é sempre x-1
+#print ['USA', 'India']
